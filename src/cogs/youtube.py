7bd6788c15b1d4be6dda5e912684cba72bedd32b
Gonzalo Bonora <bonora@strands.com>
2021-10-11 04:24:24 -0300

    Play queue implemented

--- src/cogs/youtube.py
+++ src/cogs/youtube.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import discord
 from discord.ext import commands
 
@@ -19,11 +21,13 @@ class Youtube(commands.Cog, name="Youtube"):
 
         server = self.get_server(ctx)
         await self.join(ctx)
-        voice_channel = self.get_voice_client(ctx)
+        voice_client = self.get_voice_client(ctx)
 
         try:
             async with ctx.typing():
-                await self.file_queue.add_url(url)  # get from queue
+                await self.file_queue.add_url(url)
+                await ctx.send("Song added to queue: {}".format(url))
+            if not voice_client.is_playing():
                 await self.start_music(ctx)
         except Exception as e:
             print(e)
@@ -51,13 +55,18 @@ class Youtube(commands.Cog, name="Youtube"):
     async def start_music(self, ctx):
         voice_client = self.get_voice_client(ctx)
         filename = self.file_queue.get_next()
-        voice_client.play(discord.FFmpegPCMAudio(executable="ffmpeg", source=filename), after=self.check_queue)
+        voice_client.play(discord.FFmpegPCMAudio(executable="ffmpeg", source=filename),
+                          after=lambda x: self.check_queue(ctx))
         await ctx.send('**Now playing:** {}'.format(filename))
 
     def check_queue(self, ctx):
         print("CHECKING QUEUE:")
         if not self.file_queue.is_empty():
-            self.start_music(ctx)
+            voice_client = self.get_voice_client(ctx)
+            filename = self.file_queue.get_next()
+            voice_client.play(discord.FFmpegPCMAudio(executable="ffmpeg", source=filename),
+                              after=lambda x: self.check_queue(ctx))
+            asyncio.run_coroutine_threadsafe(ctx.send("No more songs in queue."), self.bot.loop)
         print("NO MORE MUSIC")
 
     def get_server(self, ctx):
